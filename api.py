57bb8adc669683c5344f5591294e21478597e401
Pedrohenyk <158170959+Pedrohenyk@users.noreply.github.com>
2025-09-26 15:22:11 -0300

    api.py

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ load_dotenv(dotenv_path=dotenv_path)
 app = Flask(__name__)
 
 # Pega a URL exata do seu site na Vercel
-vercel_url = "https://organizador-select-com-4kr60uv7p-pedro-s-projects-e5b2839e.vercel.app"
+vercel_url = "https://organizador-select-com-ia.vercel.app"
 CORS(app, resources={r"/api/*": {"origins": vercel_url}})
 
 db_uri = os.getenv('DATABASE_URL')
@@ -125,4 +125,5 @@ def delete_card(card_id):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
+
 
